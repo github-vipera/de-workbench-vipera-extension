63c93d1515a4a3802054533cd2d28e1dca101234
Marco Bonati <marco.bonati@vipera.com>
2017-10-09 14:33:19 +0200

    Fixed tsconfig

--- lib/utils/DEUtils.d.ts
+++ lib/utils/DEUtils.d.ts
@@ -1,10 +1,4 @@
 export declare class DEUtils {
-    /**
-     * Returns TRUE if the DE CLI seems to be available
-     */
     static checkForDECli(): Promise<boolean>;
-    /**
-     * installs globally the DE CLI
-     */
     static installDECli(): Promise<boolean>;
 }
